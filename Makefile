aa4b353b000d5ab1c55e63846ea1047de7061c2f
U-DESKTOP-D7M69HC\Kevin <kevinholmes88@gmail.com>
2018-06-19 16:58:34 -0400

    Reverting back to previous build, PIE still caused issues

--- Makefile
+++ Makefile
@@ -57,7 +57,7 @@ ROMFS		:=	game
 
 APP_TITLE	:= LaiNes
 APP_AUTHOR	:= Kevoot
-APP_VERSION	:= 1.2
+APP_VERSION	:= 1.0
 APP_TITLEID	:= 1248
 
 ICON		:= icon.jpg
@@ -65,20 +65,19 @@ ICON		:= icon.jpg
 #---------------------------------------------------------------------------------
 # options for code generation
 #---------------------------------------------------------------------------------
-ARCH	:=	-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC
+ARCH	:=	-march=armv8-a -mtune=cortex-a57 -mtp=hard -fpic -pie
 
-CFLAGS	:=	-g -Wall -O3 -ffunction-sections -fpermissive  #-ffast-math -funsafe-math-optimizations 
-CFLAGS  +=	`sdl2-config --cflags` `freetype-config --cflags` 
-CFLAGS  +=	$(ARCH) $(DEFINES)
+CFLAGS	:=	-g -Wall -O3 -ffunction-sections -fpermissive -ffast-math -funsafe-math-optimizations `sdl2-config --cflags` `freetype-config --cflags` \
+			$(ARCH) $(DEFINES)
 
 CFLAGS	+=	$(INCLUDE) -D__SWITCH__
 
-CXXFLAGS	:= $(CFLAGS) -fno-rtti -fexceptions -std=gnu++11 #-shared -static
+CXXFLAGS	:= $(CFLAGS) -fno-rtti -fexceptions -std=gnu++17 -shared -static
 
 ASFLAGS	:=	-g $(ARCH)
 LDFLAGS	=	-g $(ARCH) -Wl,-Map,$(notdir $*.map)
 
-LIBS := -lSDL2_mixer -lSDL2_ttf -lSDL2_gfx -lSDL2_image -lpng -ljpeg `sdl2-config --libs` `freetype-config --libs`
+LIBS := -lSDL2_mixer -lmodplug -lmpg123 -lSDL2_ttf -lSDL2_gfx -lSDL2_image -lpng -ljpeg `sdl2-config --libs` `freetype-config --libs`
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
--- source/gui.cpp
+++ source/gui.cpp
@@ -1,12 +1,7 @@
 #include <csignal>
-#include <dirent.h>
-#include <time.h>
-
-#include <switch.h>
-#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-
+#include <SDL2/SDL.h>
 #include "Sound_Queue.h"
 #include "apu.hpp"
 #include "cartridge.hpp"
@@ -14,6 +9,8 @@
 #include "menu.hpp"
 #include "gui.hpp"
 #include "config.hpp"
+#include <dirent.h>
+#include <switch.h>
 
 namespace GUI {
 
@@ -45,7 +42,6 @@ FileMenu* fileMenu;
 bool pause = true;
 int currentRenderQuality = 0;
 bool exitFlag = false;
-Uint32 nextTime;
 
 /*void set_render_quality(int quality) {
     switch(quality) {
@@ -110,7 +106,7 @@ void updateVideoMenu() {
 void init()
 {
     // Initialize graphics system:
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK | SDL_INIT_TIMER) < 0) {
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) < 0) {
         printf("Failed to init video, audio, and joystick\n");
         return;
     }
@@ -146,7 +142,7 @@ void init()
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
-    // SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT);
+    SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT);
 
     gameTexture = SDL_CreateTexture(renderer,
                                     SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
@@ -283,7 +279,7 @@ void new_samples(const blip_sample_t* samples, size_t count)
 /* Render the screen */
 void render()
 {
-    // SDL_RenderClear(renderer);
+    SDL_RenderClear(renderer);
 
     // Draw the NES screen:
     if (Cartridge::loaded())
